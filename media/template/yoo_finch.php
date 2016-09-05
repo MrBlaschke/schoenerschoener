1a1de993184b61d6380f6fc8f85545a12aae33d3
Frank Price <github@mrblaschke.de>
2016-09-05 13:41:07 +0200

    kleiner Test

--- media/template/yoo_finch.php
+++ media/template/yoo_finch.php
@@ -1,6 +1,6 @@
 {
-    "tmpl_check": "d19986025225d239ff7119fc1bba4c26",
+    "tmpl_check": "083d7dac2127c4247bc85331760fc945",
     "tmpl_data": "{\"status\":\"up-to-date\",\"message\":\"You're up to date!. 1.0.5 is currently the newest version available.\"}",
-    "warp_check": "5bcbbcd7136fc6dd081a101bb34ba53d",
+    "warp_check": "1d9135f11aa54c25653e9a6fea89478f",
     "warp_data": "{\"status\":\"up-to-date\",\"message\":\"You're up to date!. 7.3.28 is currently the newest version available.\"}"
 }
