1500061e4a7f103f472c23551ddac613c447b4f5
Frank Price <github@mrblaschke.de>
2016-09-05 13:34:58 +0200

    erster commit schoener-schoener beta 3

---
+++ media/template/yoo_finch.php
@@ -0,0 +1,6 @@
+{
+    "tmpl_check": "d19986025225d239ff7119fc1bba4c26",
+    "tmpl_data": "{\"status\":\"up-to-date\",\"message\":\"You're up to date!. 1.0.5 is currently the newest version available.\"}",
+    "warp_check": "5bcbbcd7136fc6dd081a101bb34ba53d",
+    "warp_data": "{\"status\":\"up-to-date\",\"message\":\"You're up to date!. 7.3.28 is currently the newest version available.\"}"
+}
